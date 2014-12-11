295047c10b00db18b0d44def75c621cab2c7a9d4
amcauley <amcauley@ucla.edu>
2014-12-11 02:59:11 -0800

    Faster drawing, smoother transitions, better redraw
    Drawing is sped up. Removed integer rounding on some length
    calculations, which is resulting in smoother connections between curves.
    Also modified redraw so a new triangle can be started before the old one
    finishes (the old one will finish its drawing using a hidden turtle -
    t1). Still needs cleanup and comments.

--- cwTriangle.py
+++ cwTriangle.py
@@ -8,22 +8,42 @@ PARAM_TRIANGLE_RANGE        = 100
 PARAM_BUFFER_SIZE           = 15
 PARAM_DOT_SIZE              = 5
 PARAM_SPECIFY_CIRCLE_PTS    = None
+PARAM_PENSIZE               = 2
 
 sc = turtle.Screen()
-t = turtle.Turtle()
+t0 = turtle.Turtle()
+t1 = turtle.Turtle()
+t = t0
+t1.hideturtle()
+t1.penup()
+t1.speed('fastest')
+cancelDrawing = 0
 
 sc.setup(PARAM_TRIANGLE_RANGE*5,PARAM_TRIANGLE_RANGE*5)
 
 def turtleReset(x,y):
     print("Turtle Reset "+str((x,y)))
-    t.reset()
-    t.hideturtle()
+    t0.reset()
+    t0.hideturtle()
+    t0.speed('fastest')
+    t0.pensize(PARAM_PENSIZE)
     ''' python doc section 24.5.4.3 shows onclick as a mdethod of the turtle class, but it actually belongs to the screen class.
         similar issue with setup    '''
     sc.onclick(turtleReset)
     cwTriangle()
 
+def tPendown(t):
+    if (t == t0):
+        t.pendown()
+                
+    
 def cwTriangle():
+
+    global t
+    t = t0
+
+    global cancelDrawing
+    cancelDrawing = 0
     
     if(PARAM_SCR_SEED != None):
         random.seed(PARAM_SCR_SEED)
@@ -52,9 +72,9 @@ def cwTriangle():
     lengths.remove(D)
     D = lengths[0] + lengths[1] - D + PARAM_BUFFER_SIZE
     
-    AA = int(.5*(D+A-B-C))
-    BB = int(.5*(D+B-A-C))
-    CC = int(.5*(D+C-A-B))
+    AA = .5*(D+A-B-C)
+    BB = .5*(D+B-A-C)
+    CC = .5*(D+C-A-B)
     
     '''from documentation, circle center is radius units to the left of turtle'''
 
@@ -63,27 +83,46 @@ def cwTriangle():
     t.setheading(0)
     
     t.penup()
+    
+    if(cancelDrawing):
+        return
+    
     t.setpos(a[0],a[1])
     t.dot(PARAM_DOT_SIZE, colors[0])
-    t.pendown()
+    tPendown(t)
     hh = t.towards(b[0],b[1])
     t.setheading(hh)
+
+    if(cancelDrawing):
+        return
+        
     t.setpos(b[0],b[1])
     t.dot(PARAM_DOT_SIZE, colors[1])
     abh = t.heading()
     bah = (abh+180)%360
     hh = t.towards(c[0],c[1])
     t.setheading(hh)    
+
+    if(cancelDrawing):
+        return
+
     t.setpos(c[0],c[1])
     t.dot(PARAM_DOT_SIZE, colors[2])
     bch = t.heading()
     cbh = (bch+180)%360
     hh = t.towards(a[0],a[1])
     t.setheading(hh)    
+
+    if(cancelDrawing):
+        return
+    
     t.setpos(a[0],a[1])
     cah = t.heading()
     ach = (cah+180)%360
     t.penup()
+    
+    if(cancelDrawing):
+        return
     
     print("abh "+str(abh)+" bah "+str(bah)+" bch "+str(bch)+
           " cbh "+str(cbh)+" cah "+str(cah)+" ach "+str(ach))
@@ -107,13 +146,22 @@ def cwTriangle():
     print("vertices: "+str(vertices))
     
     for vidx in range(0,len(vertices)):
+    
         v0 = vertices[vidx] #current vertex
         v1 = vertices[(vidx+1)%3]
         v2 = vertices[(vidx-1)%3]
+        
+        if(cancelDrawing):
+            return
+            
         t.color(colors[vidx])
+
         ''' find angle (in rad) of main arc using law of cosines '''
         theta = math.acos((pow(v1[-3],2)+pow(v2[-3],2)-pow(v0[-3],2))/(2*v1[-3]*v2[-3]))
-        theta = theta*180/3.14159
+        theta = theta*180/3.14159265
+
+        if(cancelDrawing):
+            return
         
         ''' draw minor arc behind the current vertex's angle '''
         r = v0[-2]
@@ -124,14 +172,21 @@ def cwTriangle():
         t.left(90)
         p = t.position()
         h = t.heading()
-        t.pendown()
+        tPendown(t)
         t.circle(r,-theta/2,PARAM_SPECIFY_CIRCLE_PTS)
         t.penup()
         t.setpos(p)
-        t.pendown()
+        tPendown(t)
         t.setheading(h)
+        
+        if(cancelDrawing):
+            return
+        
         t.circle(r,theta/2,PARAM_SPECIFY_CIRCLE_PTS)
         t.penup()
+
+        if(cancelDrawing):
+            return
         
         ''' draw major arc faced by this vertex's angle '''
         r = D - r
@@ -142,14 +197,22 @@ def cwTriangle():
         t.left(90)
         p = t.position()
         h = t.heading()
-        t.pendown()
+        tPendown(t)
         t.circle(r,-theta/2,PARAM_SPECIFY_CIRCLE_PTS)
         t.penup()
         t.setpos(p)
-        t.pendown()
+        tPendown(t)
         t.setheading(h)
+        
+        if(cancelDrawing):
+            return
+        
         t.circle(r,theta/2,PARAM_SPECIFY_CIRCLE_PTS)
         t.penup()
+        
+    ''' any routines that were going on before this one should be killed '''
+    cancelDrawing = 1
+    t = t1
         
     
 if __name__ == "__main__":
